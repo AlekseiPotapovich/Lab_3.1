f50bb7dd474909ad6341b8eb5f9bf143332475d0
AlekseiPotapovich <alekseipotapovich@gmail.com>
2020-04-03 01:01:25 +0300

    commit

--- src/Adres.java
+++
@@ -1,8 +0,0 @@
-
-public class Adres {
-	private String street;
-	private int house;
-	private int flat;
-	
-
-}
--- src/Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-	public static void main(String[]args ) {
-	View view = new View();
-	view.setVisible(true);
-	view.setResizable(false);
-	}
-}
